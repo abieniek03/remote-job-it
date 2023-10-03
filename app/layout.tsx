e6af81ed5365d23c76bcc9dcd3ee87ad0b3ce404
Adrian Bieniek <a.bieniek.kontakt@gmail.com>
2023-10-03 12:40:11 +0200

    Add animations to main page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Poppins } from "next/font/google";
+
+import AOSProvider from "@/hoc/AOSProvider";
 
 const font = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -13,7 +16,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="pl" className="scroll-smooth">
-			<body className={`${font.className} text-content overflow-x-hidden`}>{children}</body>
+			<Head>
+				<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+				<style>
+					<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+				</style>
+			</Head>
+			<body className={`${font.className} text-content overflow-x-hidden`}>
+				<AOSProvider>{children}</AOSProvider>
+			</body>
 		</html>
 	);
 }
--- components/SpecialistItem/SpecialistItem.tsx
+++ components/SpecialistItem/SpecialistItem.tsx
@@ -8,7 +8,11 @@ interface ISpacialistItem {
 
 export default function SpecialistItem({ image, title, description }: ISpacialistItem) {
 	return (
-		<div className="relative bg-white w-full overflow-hidden rounded-xl flex flex-col border shadow-lg">
+		<div
+			className="relative bg-white w-full overflow-hidden rounded-xl flex flex-col border shadow-lg"
+			data-aos="zoom-in"
+			data-aos-duration="1000"
+		>
 			<Image src={image} alt="" className="h-72 w-full object-cover" />
 			<div className=" w-full top-0 left-0 flex flex-col justify-center">
 				<div className="p-8">
